1bdc08ebb9200fa52e14611571b30070a294cdb3
notadevps <sahilarora8527397612@gmail.com>
2021-01-28 18:54:47 +0530

    embed

--- src/components/embeds/send.vue
+++ src/components/embeds/send.vue
@@ -12,14 +12,13 @@
             <div class="forms"> 
                 <form class="embed" v-if="mode === 'embed' "> 
                     <input type="text" name="guildId" :value="Id" readonly/> 
-                    <textarea placeholder="CONTENT"> 
+                    <textarea placeholder="CONTENT" id="embed-content"> 
                     </textarea>
                     <input type="text" name="channelId" v-model="channel" placeholder="CHANNEL ID ">   
                     <p v-if="channel.length > 2 && channel.length < 9" > Invalid Channel Id </p>
                     <input type="text" name="color" v-model="color" placeholder="COLOR">
                     <p v-if="color.length > 1 && !color.includes('#') "> Invalid Color Should Be A Hex Code </p>
-
-                    <button type="sumbit"> Sumbit </button> 
+                    <button type="sumbit" @click.prevent="sendEmbed" > Sumbit </button> 
                 </form>
                 <form class="imageembeds" v-if="mode === 'imageembed' ">
                     <input type="text" name="guildId" :value="Id" readonly>
@@ -37,8 +36,18 @@
                     <p v-if="footerimage.length > 2 &&  !footerimage.includes('http') ">Invalid Image Link. </p>
                     <input type="text" name="thumbnail" v-model="thumb" placeholder="THUMBNAIL"> 
                     <p v-if="thumb.length > 2 &&  !thumb.includes('http')"> Invalid Image Link. </p>
-                    <button type="sumbit"> Sumbit </button> 
+                    <button type="sumbit" > Sumbit </button> 
                 </form> 
+            </div>
+        </div>
+            <div id="myModal" class="modal" v-if="show">
+            <div class="modal-content">
+            <div class="modal-header">
+                <i class="fas fa-times" @click="close"></i>
+            </div>
+            <div class="modal-body">
+                <p id="loading"> {{ loading }} </p>
+            </div>
             </div>
         </div>
 </template>
@@ -73,7 +82,8 @@ export default {
             footer: '', 
             thumb: '', 
             image: '', 
-            footerimage: ''
+            footerimage: '', 
+            loading: 'Sending...'
         }
     }, 
     methods: { 
@@ -83,7 +93,51 @@ export default {
                 this.$store.dispatch('press');
             }
         }, 
-    }
+        close() {
+            this.show = false;
+                this.loading = 'Sending...';
+        },
+        async sendEmbed() { 
+            let userId = document.cookie; 
+            this.show = true;
+            if (!userId) {
+                return console.log('No Cookies Found!')
+            }
+            let cookies = document.cookie.split(';');
+            let user = cookies.findIndex(x => x.includes('userid')); 
+            if(user.length < 0 ) { 
+                return console.log('Length Smaller Than 0')
+            } 
+            let loginUserId = cookies[user].split('=')[1];
+            const content = document.querySelector('#embed-content').value;  
+            let body = { 
+                query: 
+                `
+                {
+                    sendEmbed(userData: { guildId: "${this.Id}", channelId: "${this.channel}", color: "${this.color}", description: "${content}", userId: "${loginUserId}"} )
+                }
+                
+                `
+            } 
+            let res =  await fetch('http://localhost:3000/graphql', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(body)
+            })
+            let response = await res.json(); 
+            if (response.data.sendEmbed[0] === 'Ok') { 
+                this.loading = 'Done!'; 
+                setTimeout(() => {
+                    this.show = false;
+                    this.loading = 'Sending...';
+                }, 2000)
+            } else { 
+                this.loading = response.data.sendEmbed[0]
+            }
+        }
+    },
 }
 </script>
 <style scoped>
@@ -204,5 +258,94 @@ export default {
         button { 
             width: 60%;
         }
+    }
+    .modal {
+        display: flex;
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        -webkit-animation-name: fadeIn; /* Fade in the background */
+        -webkit-animation-duration: 0.4s;
+        animation-name: fadeIn;
+        animation-duration: 0.4s
+    }
+
+    .modal-content {
+        position: absolute;
+        bottom: 0;
+        background-color: #fefefe;
+        width: 100%;
+        -webkit-animation-name: slideIn;
+        -webkit-animation-duration: 0.4s;
+        animation-name: slideIn;
+        animation-duration: 0.4s
+    }
+    @media screen and (max-width: 880px) { 
+        .fa-times { 
+            float: right;
+            text-align: right;
+            font-size: 1.5rem;
+            justify-content: right;
+            position: relative;
+            left: 98%;
+        }
+        .modal-header { 
+            height: 4vh;
+        }
+    }
+    .fa-times { 
+        float: right;
+        text-align: right;
+        font-size: 1.5rem;
+        justify-content: right;
+        position: relative;
+        left: 98%;
+    }    
+    .fa-times:hover,
+    .fa-times:focus {
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+    }
+    #loading { 
+        color: black;
+    }
+    .modal-header {
+        padding: 2px 16px;
+        background-color: #2c2f33;
+        color: white;
+    }
+
+    .modal-body {padding: 2px 16px;}
+    
+    .modal-footer {
+        padding: 2px 16px;
+        background-color: #5cb85c;
+        color: white;
+    }
+    @-webkit-keyframes slideIn {
+        from {bottom: -300px; opacity: 0} 
+        to {bottom: 0; opacity: 1}
+    }
+
+    @keyframes slideIn {
+        from {bottom: -300px; opacity: 0}
+        to {bottom: 0; opacity: 1}
+    }
+
+    @-webkit-keyframes fadeIn {
+        from {opacity: 0} 
+        to {opacity: 1}
+    }
+    
+    @keyframes fadeIn {
+        from {opacity: 0} 
+        to {opacity: 1}
     }
 </style>
