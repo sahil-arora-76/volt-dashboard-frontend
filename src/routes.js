39982c5aeac5989a7a13fa6c5bc8827c73125eb7
notadevps <sahilarora8527397612@gmail.com>
2021-02-21 22:33:25 +0530

    hh

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import {createRouter, createWebHistory } from 'vue-router';
 import main from './components/page/main.vue'
 import login from './components/login/login.vue';
-import { store } from './components/store/main';
+// import { store } from './components/store/main';
 import send  from './components/embeds/send.vue';
 import vote from './components/votes/votes.vue';
 import api from './components/api/apis';
 import docs from './components/docs/doc';
+import { document, window } from 'globalthis/implementation';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -22,39 +23,49 @@ export let isAuth  = () => {
     
 }
 router.beforeEach(async (to) => {
-    if (to.fullPath === '/login') { 
-        let cookie = document.cookie.split(';'); 
-        let index = cookie.findIndex(x => x.includes('token')); 
-        if (index >= 0 ) {
-            let index = cookie.findIndex(x => x.includes('userid')); 
-            if (index < 0 ) { 
-                return window.location = '/';
-            }
-            let userid = cookie[index]; 
-            let id = userid.split('=')[1];
-            const graphqlQuery = { 
-                query: `
-                    { 
-                        getUser(id:"${id}") {_id id  discriminator avatar username guilds { id name owner permissions permissions_new icon  } } 
-                    }
-                `
-            }
-            let res = await fetch('https://volt-back.herokuapp.com/graphql', { 
-                method: 'POST',
-                headers: { 
-                'Content-Type': 'application/json', 
-                }, 
-                body: JSON.stringify(graphqlQuery)
-            })
-            let response = await res.json(); 
-            if (response.errors) {
-                return window.location = 'https://volt-back.herokuapp.com/auth2';
-            } else { 
-                store.state.loginInfo = response; 
-            }
-        } else { 
-            return window.location = 'https://volt-back.herokuapp.com/auth2';
+    if (to.fullPath.includes('/login')) { 
+        if (!to.fullPath.split('?')) 
+        {
+            return window.location = '/'; 
         }
+        var getQuery = to.fullPath.split('?')[1]
+        var params = getQuery.split('&') 
+        let token = params[0].split("token")[1];
+        let user = params[1].split("userid=")[1];
+        document.cookie = `userid=${user}`; 
+        document.cookie = `token=${token}`;
+        // let cookie = document.cookie.split(';'); 
+        // let index = cookie.findIndex(x => x.includes('token')); 
+        // if (index >= 0 ) {
+        //     let index = cookie.findIndex(x => x.includes('userid')); 
+        //     if (index < 0 ) { 
+        //         return window.location = '/';
+        //     }
+        //     let userid = cookie[index]; 
+        //     let id = userid.split('=')[1];
+        //     const graphqlQuery = { 
+        //         query: `
+        //             { 
+        //                 getUser(id:"${id}") {_id id  discriminator avatar username guilds { id name owner permissions permissions_new icon  } } 
+        //             }
+        //         `
+        //     }
+        //     let res = await fetch('https://volt-back.herokuapp.com/graphql', { 
+        //         method: 'POST',
+        //         headers: { 
+        //         'Content-Type': 'application/json', 
+        //         }, 
+        //         body: JSON.stringify(graphqlQuery)
+        //     })
+        //     let response = await res.json(); 
+        //     if (response.errors) {
+        //         return window.location = 'https://volt-back.herokuapp.com/auth2';
+        //     } else { 
+        //         store.state.loginInfo = response; 
+        //     }
+        // } else { 
+        //     return window.location = 'https://volt-back.herokuapp.com/auth2';
+        // }
     }
 })
 export default router; 
